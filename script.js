7dd0c9ccf142aced2c491da84a70c75734d99560
Shayas.R.A <email.shayas@gmail.com>
2025-05-14 10:03:12 +0300

    get the user input and computer input

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 console.log("Rock Paper Scissors");
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+console.log(humanSelection);
+console.log(computerSelection);
+
+function getComputerChoice () {
+    let randomNumGen = Math.floor(Math.random() * 3);
+    if (randomNumGen == 0) {
+        return "rock";
+    } else if (randomNumGen == 1) {
+        return "paper";
+    } else if (randomNumGen == 2) {
+        return "scissors";
+    } else {
+        return "Error" ;
+    }
+}
+
+function getHumanChoice() {
+    let choice = prompt('Enter "Rock | Paper | Scissors', '');
+    if (choice == null) {
+        alert("cancelled");
+    }
+    choice = choice.toLowerCase();
+    if (choice != 'rock' && choice != 'paper' && choice != 'scissors') {
+        alert("Select from given options 'Rock | Paper | Scissors'");
+    } else {
+        return choice;
+    }
+}
