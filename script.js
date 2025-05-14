583e3d03e322a13cbf401cb1801bce2398c67c2d
Shayas.R.A <email.shayas@gmail.com>
2025-05-14 15:44:45 +0300

    Rock paper Scissors can now be played on console for 5 round and it will declare the winner

--- script.js
+++ script.js
@@ -2,12 +2,23 @@ console.log("Rock Paper Scissors");
 
 let humanScore = 0;
 let computerScore = 0;
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-console.log("Human choose " + humanSelection);
-console.log("Computer choose " + computerSelection);
+playGame();
 
-playRound(humanSelection, computerSelection);
+console.log("Computer Score " + computerScore);
+console.log("Human Score " + humanScore)
+if (humanScore > computerScore) {
+    console.log("Human win"); 
+} else if (computerScore > humanScore) {
+    console.log("Computer win");
+} else {
+    console.log("Tie");
+}
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice ());
+    }
+}
 
 function getComputerChoice () {
     let randomNumGen = Math.floor(Math.random() * 3);
@@ -30,43 +41,46 @@ function getHumanChoice() {
     choice = choice.toLowerCase();
     if (choice != 'rock' && choice != 'paper' && choice != 'scissors') {
         alert("Select from given options 'Rock | Paper | Scissors'");
+        return getHumanChoice();
     } else {
         return choice;
     }
 }
 
-function playRound(humanChoice, computerChoice) {
-  // your code here!
-  console.log("This function");
-  if (humanChoice == computerChoice) {
-    console.log("It's tie!");
-    humanScore += 0;
-    computerScore += 0;
-  } else if (humanChoice == 'rock' && computerChoice == 'paper') {
-    console.log("Human loose & Computer win");
-    humanScore += 0;
-    computerScore += 1;
-  } else if (humanChoice == 'rock' && computerChoice == 'scissors') {
-    console.log("Human win & Computer loose");
-    humanScore += 1;
-    computerScore += 0;
-  } else if (humanChoice == 'paper' && computerChoice == 'rock') {
-    console.log("Human win & Computer loose");
-    humanScore += 1;
-    computerScore += 0;
-  } else if (humanChoice == 'paper' && computerChoice == 'scissors') {
-    console.log("Human loose & Computer win");
-    humanScore += 0;
-    computerScore += 1;
-  } else if (humanChoice == 'scissors' && computerChoice == 'rock') {
-    console.log("Human loose & Computer win");
-    humanScore += 0;
-    computerScore += 1;
-  } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
-    console.log("Human win & Computer loose");
-    humanScore += 1;
-    computerScore += 0;
-  } else {
-    console.log("Something went wrong");
-  }
+function playRound(getHumanChoice, getComputerChoice) {
+    const humanChoice = getHumanChoice;
+    const computerChoice = getComputerChoice;
+    console.log("Human choose " + humanChoice);
+    console.log("Computer choose " + computerChoice);
+    if (humanChoice == computerChoice) {
+        console.log("It's tie!");
+        humanScore += 0;
+        computerScore += 0;
+    } else if (humanChoice == 'rock' && computerChoice == 'paper') {
+        console.log("Human loose & Computer win");
+        humanScore += 0;
+        computerScore += 1;
+    } else if (humanChoice == 'rock' && computerChoice == 'scissors') {
+        console.log("Human win & Computer loose");
+        humanScore += 1;
+        computerScore += 0;
+    } else if (humanChoice == 'paper' && computerChoice == 'rock') {
+        console.log("Human win & Computer loose");
+        humanScore += 1;
+        computerScore += 0;
+    } else if (humanChoice == 'paper' && computerChoice == 'scissors') {
+        console.log("Human loose & Computer win");
+        humanScore += 0;
+        computerScore += 1;
+    } else if (humanChoice == 'scissors' && computerChoice == 'rock') {
+        console.log("Human loose & Computer win");
+        humanScore += 0;
+        computerScore += 1;
+    } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
+        console.log("Human win & Computer loose");
+        humanScore += 1;
+        computerScore += 0;
+    } else {
+        console.log("Something went wrong");
+    }
 }
